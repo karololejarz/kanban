489d03ffe6c5c14c25c2cd91afea1f4603c04bd6
karololejarz <karol1olejarz@gmail.com>
2018-05-09 16:08:53 +0200

    Initial commit

---
+++ js/card.js
@@ -0,0 +1,63 @@
+function Card(id, name) {
+  var self = this;
+  
+  this.id = id;
+  this.name = name || 'No name given';
+  this.element = createCard();
+
+  function createCard() {
+    var card = $('<li class="card"></li>');
+    var cardDeleteBtn = $('<button class="btn-delete">Delete</button>');
+    var cardEditBtn = $('<button class="btn-edit-card">Edit</button>');
+    var cardDescription = $('<p class="card-description"></p>');
+    
+    cardDeleteBtn.click(function(){
+      self.removeCard();
+    });
+
+    cardEditBtn.click(function(){
+      self.editCard();
+    });
+    
+    card.append(cardDeleteBtn);
+    card.append(cardEditBtn);
+    cardDescription.text(self.name);
+    card.append(cardDescription);
+    return card;
+  }
+}
+
+Card.prototype = {
+  removeCard: function() {
+    var self = this;
+    $.ajax({
+      url: baseUrl + '/card/' + self.id,
+      method: 'DELETE',
+      success: function(){
+        self.element.remove();
+      }
+    });
+  },
+
+  editCard: function() {
+    var self = this;
+    var newName = prompt('Enter a new name of card',
+      this.element.children('.card-description').text());
+    var columnId = this.element.closest('.column').attr('id');
+    console.log($(this).closest('.column').attr('name'));
+    console.log(columnId);
+    
+    $.ajax({
+      url: baseUrl + '/card/' + self.id,
+      method: 'PUT',
+      data: {
+        id: self.id,
+        name: newName,
+        bootcamp_kanban_column_id: columnId
+      },
+      success: function(response) {
+        self.element.children('.card-description').text(newName);
+      }
+    });
+  }
+};
---
+++ js/column.js
@@ -0,0 +1,80 @@
+function Column(id, name) {
+  var self = this;
+  
+  this.id = id;
+  this.name = name || 'No name given';
+  this.element = createColumn();
+
+  function createColumn() {
+    var column = $('<div class="column"></div>');
+    var columnTitle = $('<h2 class="column-title">' + self.name + '</h2>');
+    var columnCardList = $('<ul class="column-card-list"></ul>');
+    var columnDelete = $('<button class="btn-delete">Delete column</button>');
+    var columnEdit = $('<button class="btn-edit">Edit column</button>');
+    var columnAddCard = $('<button class="column-add-card">Add card</button>');
+
+    column.attr('id', self.id);
+    column.attr('name', self.name);    
+    
+    columnDelete.click(function() {
+      self.deleteColumn();
+    });
+    columnEdit.click(function() {
+      self.editColumn();
+    });
+    
+    columnAddCard.click(function(event) {
+      var cardName = prompt("Enter the name of the card");
+      event.preventDefault();
+      $.ajax({
+        url: baseUrl + '/card',
+        method: 'POST',
+        data: {
+          name: cardName,
+          bootcamp_kanban_column_id: self.id
+        },
+        success: function(response) {
+          var card = new Card(response.id, cardName);
+          self.createCard(card);
+        }
+      });
+    });
+      
+    column.append(columnTitle)
+      .append(columnDelete)
+      .append(columnEdit)
+      .append(columnAddCard)
+      .append(columnCardList);
+      return column;
+    }
+  }
+Column.prototype = {
+  createCard: function(card) {
+    this.element.children('ul').append(card.element);
+  },
+  deleteColumn: function() {
+    var self = this;
+    $.ajax({
+      url: baseUrl + '/column/' + self.id,
+      method: 'DELETE',
+      success: function(response){
+        self.element.remove();
+      }
+    });
+  },
+  editColumn: function() {
+    var self = this;
+    var newColumnName = prompt('Enter a new name of the column', this.element.children("h2").text());
+    $.ajax({
+      url: baseUrl + '/column/' + self.id,
+      method: 'PUT',
+      data: {
+        id: self.id,
+        name: newColumnName,
+      },
+        success: function(response){
+          self.element.children('h2').text(newColumnName);
+        }
+    });
+  }
+};
